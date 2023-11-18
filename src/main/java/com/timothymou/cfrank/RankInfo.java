e6847b8445a2012891c97b7db132283a57c26ef4
Timothy Mou <timothymou7@gmail.com>
2023-11-18 10:50:26 -0600

    Change from map to offset-list in RankInfo

--- src/main/java/com/timothymou/cfrank/RankInfo.java
+++ src/main/java/com/timothymou/cfrank/RankInfo.java
@@ -9,13 +9,13 @@ import java.util.function.Function;
 // Answers the question "what global rank would a rating R be after contest C"?
 public class RankInfo {
     private static final int MIN_RATING = -100;
-    private static final int MAX_RATING = 5000; // add some buffer for tourist :)
+    private static final int MAX_RATING = 4200; // add some buffer for tourist :)
 
     private static int clamp(int x) {
         return Math.max(MIN_RATING, Math.min(MAX_RATING, x));
     }
 
-    private final Map<Integer, Map<Integer, Integer>> contestIdToRanks;
+    private final Map<Integer, List<Integer>> contestIdToRanks;
     private final Map<String, Integer> handleToCurrentRating;
 
     private final Deque<Contest> contestStack;
@@ -25,18 +25,18 @@ public class RankInfo {
         return contestStack.isEmpty() ? null : contestStack.peekFirst();
     }
 
-    private static Map<Integer, Integer> rankToFreq(Map<Integer, Integer> e) {
-        Map<Integer, Integer> f = new HashMap<>(e);
+    private static ArrayList<Integer> rankToFreq(List<Integer> e) {
+        ArrayList<Integer> f = new ArrayList<>(e);
         for (int i = MAX_RATING - 1; i >= MIN_RATING; i--) {
-            f.put(i, e.get(i) - e.get(i + 1));
+            f.set(i - MIN_RATING, e.get(i - MIN_RATING) - e.get(i + 1 - MIN_RATING));
         }
         return f;
     }
 
-    private static Map<Integer, Integer> freqToRank(Map<Integer, Integer> f) {
-        Map<Integer, Integer> e = new HashMap<>(f);
+    private static ArrayList<Integer> freqToRank(List<Integer> f) {
+        ArrayList<Integer> e = new ArrayList<>(f);
         for (int i = MAX_RATING - 1; i >= MIN_RATING; i--) {
-            e.put(i, e.get(i + 1) + e.get(i));
+            e.set(i - MIN_RATING, e.get(i + 1 - MIN_RATING) + e.get(i - MIN_RATING));
         }
         return e;
     }
@@ -61,26 +61,25 @@ public class RankInfo {
     public void addContest(Contest contest, List<RatingChange> ratingChanges) {
         assert (!contestIdToRanks.containsKey(contest.getId()));
         assert (contestStack.isEmpty() || contest.getStartTime() >= contestStack.peekFirst().getStartTime());
-        Map<Integer, Integer> frequencies;
+        ArrayList<Integer> frequencies;
         if (!contestStack.isEmpty()) {
             frequencies = rankToFreq(contestIdToRanks.get(contestStack.peekFirst().getId()));
         } else {
-            frequencies = new HashMap<>();
-            for (int i = MIN_RATING; i <= MAX_RATING; i++) frequencies.put(i, 0);
+            frequencies = new ArrayList<>(Collections.nCopies(MAX_RATING - MIN_RATING + 1, 0));
         }
         for (RatingChange r : ratingChanges) {
-            Integer oldRating = clamp(r.getOldRating());
-            Integer newRating = clamp(r.getNewRating());
+            int oldRating = clamp(r.getOldRating());
+            int newRating = clamp(r.getNewRating());
             if (!firstContest.containsKey(r.getHandle()) ||
                     r.getContest().getStartTime() <= firstContest.get(r.getHandle()).getStartTime()) {
                 firstContest.put(r.getHandle(), r.getContest());
             } else {
-                frequencies.put(oldRating, frequencies.get(oldRating) - 1);
+                frequencies.set(oldRating - MIN_RATING, frequencies.get(oldRating - MIN_RATING) - 1);
             }
-            frequencies.put(newRating, frequencies.get(newRating) + 1);
+            frequencies.set(newRating - MIN_RATING, frequencies.get(newRating - MIN_RATING) + 1);
             handleToCurrentRating.put(r.getHandle(), newRating);
         }
-        Map<Integer, Integer> newRanks = freqToRank(frequencies);
+        List<Integer> newRanks = freqToRank(frequencies);
         contestIdToRanks.put(contest.getId(), newRanks);
         contestStack.addFirst(contest);
     }
@@ -101,6 +100,6 @@ public class RankInfo {
     }
 
     public Integer queryRank(Integer contestId, Integer rating) {
-        return contestIdToRanks.get(contestId).get(rating);
+        return contestIdToRanks.get(contestId).get(rating - MIN_RATING);
     }
 }
